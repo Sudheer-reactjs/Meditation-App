adb354bff72f750ff7730586526e54cc9c26dfbd
sahilauspicioussoft <sahil@auspicioussoft.com>
2025-03-19 12:09:06 +0530

    add Stripe and S3 configuration, implement add company form in admin panel, and update admin profile page styles

--- src/app/admin/admin-profile/page.tsx
+++ src/app/admin/admin-profile/page.tsx
@@ -39,8 +39,8 @@ const Page = () => {
     <div className="flex flex-1 flex-col gap-4">
      <div className="space-y-6">
             <div>
-              <p className="mb-4 text-zinc-300 text-base font-normal">Profile Image</p>
-              <div className="flex flex-row items-end gap-3">
+              <p className="mb-4 text-zinc-300 text-base SF Pro Display font-normal">Profile Image</p>
+              <div className="flex flex-row flex-wrap items-end gap-3">
               <Image
               src={profileImage || image}
               alt="Profile Image"
@@ -50,7 +50,7 @@ const Page = () => {
             <label htmlFor="profileImageUpload">
               <Button
                 variant="outline"                
-                className=" bg-slate-900 border-[#D7D7D7] h-8 text-zinc-300 text-sm hover:bg-slate-800"
+                className=" bg-slate-900 border-[#D7D7D7] h-8 dm-sans text-zinc-300 text-sm hover:bg-slate-800"
               >
                 Change image
               </Button>
@@ -68,55 +68,55 @@ const Page = () => {
             
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="space-y-2">
-                <Label htmlFor="firstName" className="text-white text-base font-normal">First Name</Label>
+                <Label htmlFor="firstName" className="text-white dm-sans text-base font-normal">First Name</Label>
                 <Input
                   id="firstName"
                   type='text'
                   value={formData.firstName}
                   onChange={(e) => handleChange('firstName', e.target.value)}
-                  className="bg-[#1B2236] border-none"
+                  className="bg-[#1B2236] h-12 border-none"
                 />
               </div>
               
               <div className="space-y-2">
-                <Label htmlFor="lastName" className="text-white text-base font-normal">Last Name</Label>
+                <Label htmlFor="lastName" className="text-white dm-sans text-base font-normal">Last Name</Label>
                 <Input
                   id="lastName"
                   type='text'
                   value={formData.lastName}
                   onChange={(e) => handleChange('lastName', e.target.value)}
-                  className="bg-[#1B2236] border-none"
+                  className="bg-[#1B2236] h-12 border-none"
                 />
               </div>
             </div>
             
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="space-y-2">
-                <Label htmlFor="email" className="text-white text-base font-normal">Email Address</Label>
+                <Label htmlFor="email" className="text-white dm-sans text-base font-normal">Email Address</Label>
                 <Input
                   id="email"
                   type='email'
                   value={formData.email}
                   onChange={(e) => handleChange('email', e.target.value)}
-                  className="bg-[#1B2236] border-none"
+                  className="bg-[#1B2236] h-12 border-none"
                 />
               </div>
               
               <div className="space-y-2">
-                <Label htmlFor="email" className="text-white text-base font-normal">Gender</Label>
+                <Label htmlFor="email" className="text-white dm-sans text-base font-normal">Gender</Label>
                 <Input
                   id="gender"
                   type='text'
                   value={formData.gender}
                   onChange={(e) => handleChange('gender', e.target.value)}
-                  className="bg-[#1B2236] border-none"
+                  className="bg-[#1B2236] h-12 border-none"
                 />
               </div>
             </div>
             
             <div>
               <Button 
-                className="mt-4 bg-blue-600 hover:bg-blue-700 text-white" 
+                className="mt-4 bg-[#1A3F70] w-28 h=11 hover:bg-[#1A3F70] dm-sans text-white" 
                 onClick={handleSave}
               >
                 Save
